9c913c794315c45e3f413d3234c434775143d482
Vaibhav Tade <113971081+tade2003@users.noreply.github.com>
2025-02-15 09:21:40 +0530

    Components

--- 12Blog/src/components/index.js
+++ 12Blog/src/components/index.js
@@ -3,7 +3,7 @@ import Footer from './Footer/Footer'
 import Container from './container/Container'
 import Logo from "./Logo"
 import LogoutBtn from './Header/LogoutBtn'
-import Button from './container/Button'
+import Button from './Button'
 import Input from './Input'
 import Select from './Select'
 import PostCard from './PostCard'
