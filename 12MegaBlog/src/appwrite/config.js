eb8e381e7c59eb03ef28b8965e731070a2e77d89
Vaibhav Tade <113971081+tade2003@users.noreply.github.com>
2025-02-24 18:50:02 +0530

    Blog

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -1,42 +1,39 @@
-import conf from "../conf/conf";
-import {Client, ID, Databases, Storage, Query} from "appwrite";
+import conf from '../conf/conf.js';
+import { Client, ID, Databases, Storage, Query } from "appwrite";
 
 export class Service{
     client = new Client();
     databases;
     bucket;
-
-    
     
     constructor(){
         this.client
-            .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId);
-        // this.account = new Account(this.client);
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
         this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
     }
 
-    async createPost({title,slug,content, featuredImage, status, userId}){
+    async createPost({title, slug, content, featuredImage, status, userId}){
         try {
             return await this.databases.createDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug,
                 {
-                   title,
-                   content,
-                   featuredImage,
-                   status,
-                   userId, 
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
                 }
             )
         } catch (error) {
-            console.log("Appwrite service :: createPost :: error" , error)
+            console.log("Appwrite serive :: createPost :: error", error);
         }
     }
 
-    async updatePost(slug, {title,content, featuredImage, status}){
+    async updatePost(slug, {title, content, featuredImage, status}){
         try {
             return await this.databases.updateDocument(
                 conf.appwriteDatabaseId,
@@ -47,23 +44,25 @@ export class Service{
                     content,
                     featuredImage,
                     status,
+
                 }
             )
         } catch (error) {
-            console.log("Appwrite service :: updatePost :: error",error)
+            console.log("Appwrite serive :: updatePost :: error", error);
         }
     }
 
     async deletePost(slug){
         try {
-            return await this.databases.deleteDocument(
+            await this.databases.deleteDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug
+            
             )
-            return true;
+            return true
         } catch (error) {
-            console.log("Appwrite service :: deletePost :: error",error)
+            console.log("Appwrite serive :: deletePost :: error", error);
             return false
         }
     }
@@ -73,10 +72,11 @@ export class Service{
             return await this.databases.getDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
-                slug,
+                slug
+            
             )
         } catch (error) {
-            console.log("Appwrite service :: getPost :: error",error)
+            console.log("Appwrite serive :: getPost :: error", error);
             return false
         }
     }
@@ -87,14 +87,16 @@ export class Service{
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 queries,
+                
+
             )
         } catch (error) {
-            console.log("Appwrite service :: getPosts :: error",error)
-            return false;
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
         }
     }
 
-    //File upload service
+    // file upload service
 
     async uploadFile(file){
         try {
@@ -104,7 +106,7 @@ export class Service{
                 file
             )
         } catch (error) {
-            console.log("Appwrite service :: uploadFile ::error",error)
+            console.log("Appwrite serive :: uploadFile :: error", error);
             return false
         }
     }
@@ -117,10 +119,9 @@ export class Service{
             )
             return true
         } catch (error) {
-            console.log("Appwrite service :: deleteFile :: error",error)
+            console.log("Appwrite serive :: deleteFile :: error", error);
             return false
         }
-        
     }
 
     getFilePreview(fileId){
@@ -129,9 +130,8 @@ export class Service{
             fileId
         )
     }
-} 
+}
 
 
 const service = new Service()
-
 export default service
--- 12MegaBlog/src/components/PostCard.jsx
+++ 12MegaBlog/src/components/PostCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import service from "../appwrite/config"
+import appwriteService from "../appwrite/config"
 import {Link} from 'react-router-dom'
 
 function PostCard({$id, title, featuredImage}) {
     
-    return (
+  return (
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
-                <img src={service.getFilePreview(featuredImage)} alt={title}
+                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
                 className='rounded-xl' />
 
             </div>
@@ -17,7 +17,7 @@ function PostCard({$id, title, featuredImage}) {
             >{title}</h2>
         </div>
     </Link>
-    )
+  )
 }
 
 
